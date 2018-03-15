8e6172a4836dfe99f5129efe4442a145df9ce363
jungmintaeng <jungmin4025@gmail.com>
2018-03-15 17:36:36 +0900

    2018-03-15 live coding

---
+++ src/com/cafe24/helloweb/servlet/LiftCycleServlet.java
@@ -0,0 +1,42 @@
+package com.cafe24.helloweb.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/lc")
+public class LiftCycleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LiftCycleServlet() {
+        super();
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init called");
+		super.init();
+	}
+
+	public void destroy() {
+		System.out.println("destroy called");
+		super.destroy();
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("service called");
+		super.service(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet called");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost called");
+	}
+
+}
